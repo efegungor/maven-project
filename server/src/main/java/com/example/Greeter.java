69796e355753df0efbd4a9190d450b945b02cf71
Efe <Efe@Efe-G.local>
2018-01-27 11:36:39 +0200

    efe

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,8 +11,10 @@ public class Greeter {
   public Greeter() {
 
   }
-
-  public String greet(String someone) {
+/*
+* Comment
+*/
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
